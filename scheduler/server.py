da3d17f50553f842863c163332c1cd7ae2372abf
Jacob Lawson <j.lawson@bullhorn.com>
2017-04-08 23:22:38 -0400

    mile marker before logging.  need to get alert disappearance working.  Employee addition and display is working.

--- scheduler/server.py
+++ scheduler/server.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, jsonify, request
 from creds import creds
 from modules.models import db
 from modules.models.employee import Employee
 from modules.models.day import Day
+from sqlalchemy import exc
 
 
 app = Flask(__name__)
@@ -27,12 +27,36 @@ def render_partial(partial):
     return render_template('partials/{}'.format(partial))
 
 
+@app.route("/createEmployee", methods=['POST'])
+def create_employee():
+    json = request.get_json()
+    first_name = json['firstName']
+    last_name = json['lastName']
+    middle_name = json['middleName']
+    user_name = json['userName']
+    employee = Employee(first_name, last_name, middle_name, user_name)
+    db.session.add(employee)
+    try:
+        db.session.commit()
+    except exc.IntegrityError:
+        db.session.rollback()
+        return '', 403
+
+    return jsonify({'id': employee.id})
+
+
 @app.route("/getEmployees")
 def get_employees():
     employees = Employee.query.all()
     return_list = []
     for employee in employees:
-        return_list.append(employee.get_first_name())
+        return_list.append({
+            'id': employee.id,
+            'firstName': employee.first_name,
+            'lastName': employee.last_name,
+            'middleName': employee.middle_name,
+            'userName': employee.user_name
+        })
 
     return jsonify({'employees': return_list})
 
--- scheduler/static/js/controllers/ManageEmployeesController.js
+++ scheduler/static/js/controllers/ManageEmployeesController.js
@@ -3,7 +3,61 @@
     var app = angular.module('Scheduler');
 
     app.controller('ManageEmployeesController', function ($scope, $http) {
+        $scope.showCreateEmployee = false;
+        $scope.firstName = null;
+        $scope.lastName = null;
+        $scope.middleName = null;
+        $scope.userName = null;
 
+        $scope.init = function () {
+            getEmployees();
+        };
+
+        var getEmployees = function () {
+            $http.get('/getEmployees')
+                .then(function (response) {
+                    $scope.employees = response.data.employees;
+                })
+        };
+
+        $scope.createEmployee = function () {
+            $scope.showAlert = false;
+            $scope.message = '';
+
+            if(!$scope.firstName || !$scope.lastName || !$scope.userName) {
+                $scope.showAlert = true;
+                $scope.message = 'Please fill in all required fields';
+                return;
+            }
+            var params = {
+                    firstName: $scope.firstName,
+                    lastName: $scope.lastName,
+                    middleName: $scope.middleName,
+                    userName: $scope.userName
+            };
+
+            $http.post('/createEmployee', params)
+                .then(function (response) {
+                    $scope.showCreateEmployee = false;
+                    $scope.showAlert = true;
+                    $scope.message = 'Successfully Created Employee ' + $scope.lastName + ', ' + $scope.firstName + ' ';
+                    setTimeout(function () {
+                        $scope.showAlert = false;
+                        $scope.message = '';
+                    }, 3000);
+                    getEmployees();
+                },function (response) {
+                    if (response.status == 403) {
+                        $scope.userName = null;
+                        $scope.showAlert = true;
+                        $scope.message = 'User Name already taken';
+                    }
+                })
+        };
+
+        $scope.toggleShowCreateEmployee = function () {
+            $scope.showCreateEmployee = !$scope.showCreateEmployee;
+        }
     })
 
 } () );
